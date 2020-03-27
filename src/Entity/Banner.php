4bb3f309c9be904e29908777b5c68220f9db5704
vladanzivanovic <vladapoz85@gmail.com>
2020-03-27 21:25:43 +0100

    managing products

--- src/Entity/Banner.php
+++ src/Entity/Banner.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BannerRepository")
  */
-final class Banner
+class Banner
 {
     public const POSITION_HOME_LEFT = 1;
 
