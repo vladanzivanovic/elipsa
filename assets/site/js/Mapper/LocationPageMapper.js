b6b86dc43f5be5329f4f8e125601cc893566285f
vladanzivanovic <vladapoz85@gmail.com>
2020-06-01 20:42:32 +0200

    change location page

--- assets/site/js/Mapper/LocationPageMapper.js
+++ assets/site/js/Mapper/LocationPageMapper.js
@@ -2,6 +2,20 @@ class LocationPageMapper {
     constructor() {
         if(!LocationPageMapper.instance) {
             this.countryOptions = '#country-list';
+            this.locationBtn = '.location-btn';
+            this.fullImageWrapper = '#sliders';
+            this.thumbImageWrapper = '#carousel';
+            this.sliders = '.slides';
+            this.title = '.spd-title';
+            this.description = '.spd-text';
+            this.workTime = '.work-time';
+            this.workTimeWeekend = '.work-time-weekend';
+            this.address = '.address-info';
+            this.email = '.email-info';
+            this.telephone = '.telephone-info';
+            this.mobileTitle = '#mobile-title';
+            this.imagesWrapper = '.images-wrapper';
+            this.mobileDropDown = '.location-drop-down';
 
             LocationPageMapper.instance = this;
         }
