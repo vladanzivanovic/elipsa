6f9e6cd683d7b2ab9cbef241ccfe995aea57e793
vladanzivanovic <vladapoz85@gmail.com>
2020-09-29 01:02:16 +0200

    fix bank payment

--- src/Controller/Unique/RenderImagesController.php
+++ src/Controller/Unique/RenderImagesController.php
@@ -61,11 +61,23 @@ class RenderImagesController extends AbstractController
         $uploadDir = $this->parameterBag->get('upload_image_dir');
 
         try {
-            return $this->imageResizer->renderImageWithFilter($uploadDir . $name, $filter);
+            $response = $this->imageResizer->renderImageWithFilter($uploadDir . $name, $filter);
+
+            $response->setPublic();
+            $response->setMaxAge(864000);
+
+            return $response;
         } catch (NotLoadableException $notLoadableException) {
             $image = $this->imageRepository->findOneBy(['name' => $name]);
 
-            return $this->imageResizer->renderImageWithFilter($uploadDir.$image->getOriginalName(), $filter);
+            $response = $this->imageResizer->renderImageWithFilter($uploadDir.$image->getOriginalName(), $filter);
+
+            $response->setPublic();
+            $response->setMaxAge(864000);
+
+
+            return $response;
+
         }
     }
 }
