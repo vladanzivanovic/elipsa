1d991a268dc50284e4e24c7541124eb5426f322c
vladanzivanovic <vladapoz85@gmail.com>
2020-10-13 15:12:51 +0200

    fix for prod

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 final class RequestListener
 {
     public function onKernelRequest(RequestEvent $event)
     {
+        $requestUri = $event->getRequest()->server->get('REQUEST_URI');
+
+        if (strpos($requestUri, '/public') === 0) {
+
+            $redirectUri = str_replace('/public', '', $requestUri);
+
+            $event->setResponse(new RedirectResponse($redirectUri !== '' ? $redirectUri : '/', 301));
+
+            return;
+        }
+
         $session = $event->getRequest()->getSession();
 
         if (false === $session->has('user')) {
