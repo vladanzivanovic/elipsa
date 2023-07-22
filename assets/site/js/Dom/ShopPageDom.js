6c31efaf14fb9d3ab9a68815d983e1c545bcc4b2
Vladan Zivanovic <vladapoz85@gmail.com>
2023-07-22 22:41:47 +0200

    fix search

--- assets/site/js/Dom/ShopPageDom.js
+++ assets/site/js/Dom/ShopPageDom.js
@@ -14,21 +14,24 @@ class ShopPageDom {
 
     generateProducts(data) {
         let html = '';
+        const modusParam = IS_MOBILE ? 2 : 3;
+        const closingRowModus = modusParam - 1;
 
         for(let i in data.products.data) {
+
             let product = data.products.data[i];
-            let oldPriceHtml = '';
             const productLink = Routing.generate(`site.product_page.${LOCALE}`, {'slug': product.slug});
             const wishListClass = product.has_wish == 1 ? 'fa-heart' : 'fa-heart-o';
 
-            if (product.discount > 0) {
-                oldPriceHtml = `<p class="sfi-old-price">-${product.price} RSD</p>`
+            if ((i % modusParam) === 0) {
+                html += '<div class="row m-l-0 m-r-0">'
             }
 
             html += `<div class="col-md-4 col-sm-6 col-xs-6">
                         <div class="single-featured-item">
                             <div class="sfi-img">
                                 <a href="${productLink}"><img src="${product.image.file}" alt="{{ default_alt_tag }}"></a>
+                                ${ this.#setBadgeHtml(product) }
                                 <div class="sfi-img-content sfi-data-content">
                                     <p class="text-capitalize">${Translator.trans('available_colors', null, 'messages', LOCALE)}:</p>
                                     <ul class="sfi-data-color">${this.productColors(product.colors)}</ul>
@@ -47,12 +50,15 @@ class ShopPageDom {
                                     <a class="sfi-name" href="${productLink}">${product.title}</a>
                                 </div>
                                 <div class="sfi-price-rating">
-                                    <p class="sfi-price text-uppercase"><span>${product.discount > 0 ? product.discount : product.price} RSD</span></p>
-                                    ${oldPriceHtml}
+                                    ${ this.#setPriceDom(product) }
                                 </div>
                             </div>
                         </div>
                     </div>`;
+
+            if ((i % modusParam) === closingRowModus) {
+                html += '</div>'
+            }
         }
 
         return html;
@@ -97,6 +103,48 @@ class ShopPageDom {
         const criteria = `<a class="btn selected-filter-btn letter-capitalize" data-name="${name}" data-value="${value}">${text}<span class="close"></span></a>`;
 
         this.mapper.searchView.append(criteria);
+    }
+
+    #setPriceDom(product) {
+        if (product.discount !== null) {
+            return `
+                <p class="sfi-old-price">-${ product.price.amount } ${ product.price.currency }</p>
+                <div class="discount-price">
+                    <p class="sfi-price text-uppercase">
+                        <span>${ product.discount.price.amount } ${ product.price.currency }</span>
+                    </p>
+                    <p class="price-saving">${ Translator.trans('saving', null, 'messages', LOCALE) } ${ product.discount.saving.amount } ${ product.discount.saving.currency }</p>
+                </div>
+            `;
+        }
+
+        return `
+            <p class="sfi-price text-uppercase">
+                <span>${ product.price.amount } ${ product.price.currency }</span>
+            </p>
+        `;
+    }
+
+    #setBadgeHtml(product) {
+        let html = '';
+
+        if (product.is_sold === true) {
+            html += `
+                <div class="product-sold">
+                    <span>${Translator.trans('sold', null, 'messages', LOCALE)}</span>
+                </div>
+            `;
+        }
+
+        if (product.discount !== null) {
+            html += `
+                <div class="sfi-img-banner">
+                    <span>-${product.discount.percentage} %</span>
+                </div>
+            `;
+        }
+
+        return html;
     }
 }
 
