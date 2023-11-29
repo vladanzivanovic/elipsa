37ae22c1484f133f0efb52bd50b2b62a7351b7d9
Vladan Zivanovic <vladapoz85@gmail.com>
2023-11-29 21:09:25 +0100

    prod release

---
+++ src/Entity/PromotionOption.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PromotionOptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PromotionOptionRepository::class)
+ */
+class PromotionOption
+{
+    const OPTION_CATEGORIES = 'categories';
+
+    const OPTION_TAGS = 'tags';
+
+    const OPTION_PRODUCTS = 'products';
+
+    const OPTION_ALL_PRODUCTS = 'applicable_all_products';
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=PromotionCoupon::class, inversedBy="promotionOptions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $promotionId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $configuration = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPromotionId(): ?PromotionCoupon
+    {
+        return $this->promotionId;
+    }
+
+    public function setPromotionId(?PromotionCoupon $promotionId): self
+    {
+        $this->promotionId = $promotionId;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getConfiguration(): ?array
+    {
+        return $this->configuration;
+    }
+
+    public function setConfiguration(array $configuration): self
+    {
+        $this->configuration = $configuration;
+
+        return $this;
+    }
+}
