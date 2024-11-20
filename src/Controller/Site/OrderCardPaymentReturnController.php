97edf420a0a9ddbd3ce44238529ce821d2357443
Vladan Zivanovic <vladapoz85@gmail.com>
2024-11-20 19:05:41 +0100

    fix response on card callback

--- src/Controller/Site/OrderCardPaymentReturnController.php
+++ src/Controller/Site/OrderCardPaymentReturnController.php
@@ -37,7 +37,7 @@ final class OrderCardPaymentReturnController extends AbstractController
 
         $this->orderHandler->save($order);
 
-        return $this->json('OK', Response::HTTP_OK);
+        return new Response('OK', Response::HTTP_NO_CONTENT);
     }
 
     #[Route(path: '/checkout/card/return/intesa/{token}', name: 'site.checkout_completed_card_return.intesa', options: ['expose' => true], methods: ['POST'])]
